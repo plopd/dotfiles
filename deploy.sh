d080c56b72476af70c909e73fcf5a3163adbaa56
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2021-04-14 22:54:52 +0200

    Deploy changes in a batch

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "\033[0;32mCommiting new updates to dotfiles to GitHub...\033[0m"
+
+\cp -r ~/.vimrc ./vim/.vimrc
+git add -A
+
+msg="Add file changes `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
